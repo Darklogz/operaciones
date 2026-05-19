c13c741aefb16879fb5ac5492c7b87f13719cd02
Leonardo <leonardomceipac@gmail.com>
2026-05-18 20:40:42 -0600

    segundo ejercicio terminado

--- Ejercicio1_2.py
+++ Ejercicio1_2.py
@@ -2,25 +2,25 @@
 import pulp
 
 # 1. Definir el problema (Minimización)
-model = pulp.LpProblem("Cloud_Optimization", pulp.LpMinimize)
+model = pulp.LpProblem("Cloud_Optimization", pulp.LpMaximize)
 
 # 2. Definir Variables (Enteras, ya que no puedes rentar media instancia)
-x1 = pulp.LpVariable("Instancia_A", lowBound=0, upBound=40, cat='Integer')
-x2 = pulp.LpVariable("Instancia_B", lowBound=0, upBound=30, cat='Integer')
+x1 = pulp.LpVariable("Esritorio", cat='Integer')
+x2 = pulp.LpVariable("Laptop", cat='Integer')
 
 # 3. Función Objetivo
-model += 10 * x1 + 30 * x2, "Costo_Total"
+model += 2000 * x1 + 4000 * x2, "Ganancia"
 
 # 4. Restricciones
-model += 2 * x1 + 4 * x2 >= 100, "CPU_Demand"
-model += 1 * x1 + 5 * x2 >= 80, "RAM_Demand"
+model += x1 + x2 <=60, "Maximo procesadores"
+model += x1 + 3*x2 <=100, "Maximo tiempo"
 
 # 5. Resolver y mostrar
 model.solve()
 
 print(f"Estado: {pulp.LpStatus[model.status]}")
-print(f"Contratar Tipo A: {x1.varValue}")
-print(f"Contratar Tipo B: {x2.varValue}")
+print(f"Armar Tipo A: {x1.varValue}")
+print(f"Armar Tipo B: {x2.varValue}")
 print(f"Costo Mínimo Diario: ${pulp.value(model.objective)}")
 
 #source .venv/bin/activate

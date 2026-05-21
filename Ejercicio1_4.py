ca3ce7bb594db69a90a87be2f64e4352fcd09d70
Leonardo <leonardomceipac@gmail.com>
2026-05-20 20:10:04 -0600

    Ejercicio 4 resuelto completamente

--- Ejercicio1_4.py
+++ Ejercicio1_4.py
@@ -5,15 +5,17 @@ import pulp
 model = pulp.LpProblem("Cloud_Optimization", pulp.LpMaximize)
 
 # 2. Definir Variables (Enteras, ya que no puedes rentar media instancia)
-x1 = pulp.LpVariable("basico", cat='Integer')
-x2 = pulp.LpVariable("profundo", cat='Integer')
+x1 = pulp.LpVariable("blade", cat='Integer')
+x2 = pulp.LpVariable("rack", lowBound=2,  cat='Integer')
 
 # 3. Función Objetivo
-model += 2 * x1 + 5 * x2, "seguridad"
+model += 10000 * x1 + 25000 * x2, "Eps"
 
 # 4. Restricciones
-model += x1 + 3*x2 <=18, "Cpu/s"
-model += x1 + x2 <=8, "Ram/s"
+model += 2*x1 + 5*x2 <=45, "Energía"
+model += x1 + 3*x2 <=24, "Bahías"
+model += 1500*x1 + 4000*x2 <=30000, "dinero"
+
 
 # 5. Resolver y mostrar
 model.solve()
@@ -21,6 +23,6 @@ model.solve()
 print(f"Estado: {pulp.LpStatus[model.status]}")
 print(f"Armar Tipo A: {x1.varValue}")
 print(f"Armar Tipo B: {x2.varValue}")
-print(f"Seguridad máxima: {pulp.value(model.objective)}")
+print(f"Ep maximo: {pulp.value(model.objective)}")
 
 #source .venv/bin/activate
